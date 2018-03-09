050c4f2ea94fa7df055b9eed769b81b6d9a92744
Jonathan Brennecke <jpbrennecke@gmail.com>
2018-03-08 21:13:02 -0800

    reorganized imports

--- tests/index.js
+++ tests/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-configure({ adapter: new Adapter() });
-
 import { Times } from '../src';
+
+configure({ adapter: new Adapter() });
 
 describe('<Times/>', () => {
   it('renders n children', () => {
